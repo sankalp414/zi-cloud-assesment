ab1d96d295fbce87fcee861bbfada149b033ed4c
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2024-07-09 09:30:39 +0000

    chat

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -78,7 +78,7 @@ const HomePage = () => {
             href="https://github.com/sankalp414"
             target="blank"
           >
-            <strong>SankalpSharma</strong>
+            <strong>Sankalp Sharma</strong>
           </a>
         </div>
       )}
